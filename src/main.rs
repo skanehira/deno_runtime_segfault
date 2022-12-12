cb385cf0d5c3ed3f26f587847f83de3028e11414
skanehira <sho19921005@gmail.com>
2022-12-12 13:38:15 +0900

    remove futures

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,27 @@
-use std::rc::Rc;
-
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
 use deno_core::{Extension, FsModuleLoader, JsRuntime, RuntimeOptions};
-use deno_runtime::permissions::Permissions;
 use deno_runtime::js;
-use futures::executor::block_on;
+use deno_runtime::permissions::Permissions;
+use std::rc::Rc;
 
 pub async fn handler() -> Result<impl IntoResponse, String> {
-    block_on(async {
-        let extensions: Vec<Extension> = vec![
-            deno_web::init::<Permissions>(deno_web::BlobStore::default(), None),
-            deno_crypto::init(None),
-        ];
+    let extensions: Vec<Extension> = vec![
+        deno_web::init::<Permissions>(deno_web::BlobStore::default(), None),
+        deno_crypto::init(None),
+    ];
 
-        let _ = JsRuntime::new(RuntimeOptions {
-            module_loader: Some(Rc::new(FsModuleLoader)),
-            startup_snapshot: Some(js::deno_isolate_init()),
-            source_map_getter: None,
-            get_error_class_fn: None,
-            shared_array_buffer_store: None,
-            compiled_wasm_module_store: None,
-            extensions,
-            ..Default::default()
-        });
+    let _ = JsRuntime::new(RuntimeOptions {
+        module_loader: Some(Rc::new(FsModuleLoader)),
+        startup_snapshot: Some(js::deno_isolate_init()),
+        source_map_getter: None,
+        get_error_class_fn: None,
+        shared_array_buffer_store: None,
+        compiled_wasm_module_store: None,
+        extensions,
+        ..Default::default()
     });
     Ok(StatusCode::OK)
 }
